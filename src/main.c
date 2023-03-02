3a0a2735140d5d5fb439b24bc6b02db6b1ce4fdc
Th3UwU <angel.storres@alumnos.udg.mx>
2023-03-02 00:55:33 -0600

    Decode png

--- src/main.c
+++ src/main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include <lodepng.h>
 
 int main()
 {
-	printf("Hello world\n");
+	uint8_t* image;
+	uint32_t width, height;
+	lodepng_decode32_file(&image, &width, &height, "../res/image.png");
+
+	for (uint32_t i = 0; i < width * height; i++)
+	{
+		if (i % width == 0)
+			printf("\n");
+
+		printf("%03u ", (image[i*4] + image[i*4+1] + image[i*4+2]) / 3 );
+	}
+
+	free(image);
 
 	return 0;
 }
